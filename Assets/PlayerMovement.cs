dff99b62120e42f3f18543e3a16ddfb5c97acf89
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-05-17 12:08:41 -0300

    Move cube with ADSW

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,16 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public Vector3 movement;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        transform.position += movement;
+        if (transform.position.y < 6)
+        {
+            transform.position += movement;
+        }
+        
         //transform.localScale += movement;
         //transform.eulerAngles += movement;
     }
